dcaf129dd0bc21093c775932c22f57edb34b2c7c
Gabriel Silva <gabiles278@gmail.com>
2024-03-26 11:29:19 -0300

    Envio do projeto

---
+++ Restaurant-Api/src/main/java/com/restaurant/a3/RestaurantApi/RestaurantApiApplication.java
@@ -0,0 +1,13 @@
+package com.restaurant.a3.RestaurantApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantApiApplication.class, args);
+	}
+
+}
---
+++ Restaurant-Api/src/test/java/com/restaurant/a3/RestaurantApi/RestaurantApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurant.a3.RestaurantApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
