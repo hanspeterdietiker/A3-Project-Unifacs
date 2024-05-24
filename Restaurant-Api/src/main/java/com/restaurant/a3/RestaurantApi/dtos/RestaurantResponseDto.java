23095975bd6a9f14ffc95654a038ae18c3c1612a
Gabriel Silva <gabiles278@gmail.com>
2024-05-23 21:25:13 -0300

    refactor<RestaurantResponseDto, Controllers>: add @jsoninclude in RestaurantResponseDto class and add crossOrigin in controllers

--- Restaurant-Api/src/main/java/com/restaurant/a3/RestaurantApi/dtos/RestaurantResponseDto.java
+++ Restaurant-Api/src/main/java/com/restaurant/a3/RestaurantApi/dtos/RestaurantResponseDto.java
@@ -1,5 +1,6 @@
 package com.restaurant.a3.RestaurantApi.dtos;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.restaurant.a3.RestaurantApi.models.address.Address;
 import jakarta.persistence.Column;
 import lombok.*;
@@ -11,6 +12,7 @@ import java.util.List;
 @Getter
 @Setter
 @ToString
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class RestaurantResponseDto {
     private Long id;
     private String name;
